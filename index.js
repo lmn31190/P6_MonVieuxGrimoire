20889ce54622ba76fbd1c126b5b406f596777e60
lmn31190 <monielouis31@gmail.com>
2024-07-29 17:50:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+//DB CONNECT && CONFIG
+const app = express();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    throw err;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconneced");
+});
+
+//Middleware
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+
+// SERVER RUN
+app.listen(process.env.PORT, () => {
+  connect();
+  console.log(`Server running on PORT ${process.env.PORT}`);
+});
