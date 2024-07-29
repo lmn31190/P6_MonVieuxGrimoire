c1b913ce6d9fd4bece4dec2cfbf9311c395a961d
lmn31190 <monielouis31@gmail.com>
2024-07-29 19:08:19 +0200

    books models

---
+++ models/book.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+// Schéma Book
+const bookSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  ratings: [
+    {
+        userId: { type: String },
+        grade: { type: Number },
+    }
+  ],
+  averageRating: { type: Number },
+});
+
+export default mongoose.model('Book', bookSchema);
