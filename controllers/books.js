bb2ec68ffd724169be92ea2965929afc2566e6fb
lmn31190 <monielouis31@gmail.com>
2024-08-07 18:13:19 +0200

    update book

--- controllers/books.js
+++ controllers/books.js
@@ -35,39 +35,80 @@ export const getBook = async (req, res) => {
   try {
     const book = await Book.findById(req.params.id);
     if (!book) {
-      return res.status(404).json({ message: 'Livre non trouvé' });
+      return res.status(404).json({ message: "Livre non trouvé" });
     }
     res.status(200).json(book);
   } catch (err) {
     res.status(404).json({ err });
   }
 };
+
+// DELETE => Delete Book
 export const deleteBook = async (req, res) => {
-    try {
-      const book = await Book.findById(req.params.id);
-      if (!book) {
-        return res.status(404).json({ message: 'Livre non trouvé' });
-      }
-  
-      if (book.userId !== req.auth.userId) {
-        return res.status(403).json({ message: "403: Vous devez être l'auteur" });
-      }
-  
-      const filename = book.imageUrl.split('/images/')[1];
-      fs.unlink(`images/${filename}`, async (err) => {
-        if (err) {
-          return res.status(500).json({ err: err.message });
-        }
-  
-        try {
-          await Book.deleteOne({ _id: req.params.id });
-          res.status(200).json({ message: 'Livre supprimé !' });
-        } catch (err) {
-          res.status(400).json({ err: err.message });
-        }
-      });
-    } catch (err) {
-      res.status(500).json({ err: err.message });
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Livre non trouvé" });
     }
-  };
-  
+
+    if (book.userId !== req.auth.userId) {
+      return res.status(403).json({ message: "403: Vous devez être l'auteur" });
+    }
+
+    const filename = book.imageUrl.split("/images/")[1];
+    fs.unlink(`images/${filename}`, async (err) => {
+      if (err) {
+        return res.status(500).json({ err: err.message });
+      }
+
+      try {
+        await Book.deleteOne({ _id: req.params.id });
+        res.status(200).json({ message: "Livre supprimé !" });
+      } catch (err) {
+        res.status(400).json({ err: err.message });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+};
+
+// PUT => Update book
+export const updateBook = async (req, res) => {
+  try {
+    const bookData = req.file
+      ? {
+          ...JSON.parse(req.body.book),
+          imageUrl: `${req.protocol}://${req.get("host")}/images/optimized_${
+            req.file.filename
+          }`,
+        }
+      : { ...req.body };
+
+    delete bookData._userId;
+
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Livre non trouvé" });
+    }
+
+    if (book.userId !== req.auth.userId) {
+      return res.status(403).json({ message: "403: unauthorized request" });
+    }
+
+    const filename = book.imageUrl.split("/images/")[1];
+    if (req.file) {
+      fs.unlink(`images/${filename}`, (err) => {
+        if (err) console.log(err);
+      });
+    }
+
+    await Book.updateOne(
+      { _id: req.params.id },
+      { ...bookData, _id: req.params.id }
+    );
+    res.status(200).json({ message: "Objet modifié !" });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
--- routes/book.js
+++ routes/book.js
@@ -1,16 +1,21 @@
-import express from 'express';
+import express from "express";
 
-import {addBook, deleteBook, getBook, getBooks} from '../controllers/books.js';
-import upload, { resizeImage } from '../middleware/upload.js';
-import { authMiddleware } from '../middleware/auth.js';
-
+import {
+  addBook,
+  deleteBook,
+  getBook,
+  getBooks,
+  updateBook,
+} from "../controllers/books.js";
+import upload, { resizeImage } from "../middleware/upload.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get('/', getBooks); //All Books
-router.get('/:id', getBook); //One Book
-router.post('/', upload, resizeImage, addBook); // Create
-router.delete('/:id', authMiddleware, deleteBook); // Delete
-
+router.get("/", getBooks); //All Books
+router.get("/:id", getBook); //One Book
+router.post("/", upload, resizeImage, addBook); // Create
+router.delete("/:id", authMiddleware, deleteBook); // Delete
+router.put("/:id", authMiddleware, upload, resizeImage, updateBook); // Update
 
 export default router;
