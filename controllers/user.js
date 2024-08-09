41eb37f7cb1bb5d35b33665f7df39bf5b216ba49
lmn31190 <monielouis31@gmail.com>
2024-08-09 14:25:25 +0200

    auth optimisation

--- controllers/user.js
+++ controllers/user.js
@@ -3,49 +3,54 @@ import User from '../models/user.js';
 import jwt from 'jsonwebtoken';
 
 // Create account
-export const register = (req, res) => {
-    // Hash password
-    bcrypt.hash(req.body.password, 10)
+export const register = async (req, res) => {
+    try {
+        // Chiffrement du mot de passe
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-      .then(hash => {
-
-        const user = new User({
-          email: req.body.email,
-          password: hash
+        const newUser = new User({
+            email: req.body.email,
+            password: hashedPassword
         });
-        // Add MongoDB BDD
-        user.save()
-          .then(() => res.status(201).json({ message: 'Votre compte à bien été crée !' }))
-          .catch(err => res.status(400).json({ err }));
-      })
-      .catch(err => res.status(500).json({ err }));
+
+        // Enregistrement dans la base de données MongoDB
+        await newUser.save();
+        res.status(201).json({ message: 'Votre compte a été créé avec succès !' });
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            res.status(400).json({ error: 'Erreur de validation des données', details: err });
+        } else {
+            res.status(500).json({ error: 'Erreur lors de la création du compte', details: err });
+        }
+    }
 };
 
 // POST => Connexion
-export const login = (req, res) => {
-    // Vérify email
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur inconnue !' });
-            }
-            // Vérif password
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    // if valid => return user.id & token
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            process.env.JWT_SECRET,
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(err => res.status(500).json({ err }));
-        })
-        .catch(err => res.status(500).json({ err }));
+export const login = async (req, res) => {
+    try {
+       
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+
+        
+        const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Mot de passe invalide !' });
+        }
+
+        
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        );
+        res.status(200).json({
+            userId: user._id,
+            token
+        });
+    } catch (err) {
+        res.status(500).json({ err });
+    }
 };
